aa982dd8730ec8f2df261f0ba424b53ebd0dfed0
Bernard Hamann <bernard@yebolocal.com>
2015-09-16 00:31:21 +0200

    Bugfix

--- index.js
+++ index.js
@@ -1,132 +1,73 @@
 var express = require('express');
 var debug = require('debug')('express:server');
 var path = require('path');
-var ReactEngine = require('react-engine');
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
 var cors = require('cors');
 
 var nodeEngine = {};
 
-nodeEngine.init = function(){
-    return express()
-};
+nodeEngine.server = function(config){
 
-nodeEngine.reactViews = function(server){
+    var server = express();
 
-    // create an engine instance
-    var engine = ReactEngine.server.create({
-        reactRoutes:'./routes.jsx'
-    });
-
-    // view engine setup react engine
-    server.engine('.js', engine);
-
-    // set the view directory
-    server.set('views', path.join(__dirname, '/app/universal/handlers'));
-
-    // set jsx or js as the view engine
-    // (without this you would need to supply the extension to res.render())
-    // ex: res.render('index.jsx') instead of just res.render('index').
-    server.set('view engine', 'js');
-
-    // finally, set the custom view
-    server.set('view', require('react-engine/lib/expressView'));
-
-    return server
-
-};
-
-nodeEngine.serverSetup = function(server, data){
-
-    // Get port from environment and store in Express.
-    var port = normalizePort(process.env.PORT || data.env.PORT || '3000');
-    server.set('port', port);
-
-    //Normalize a port into a number, string, or false.
-    function normalizePort(val) {
-        var port = parseInt(val, 10);
-
-        if (isNaN(port)) {
-            // named pipe
-            return val;
-        }
-
-        if (port >= 0) {
-            // port number
-            console.log("App is running on Port: " + port);
-            return port;
-        }
-
-        return false;
-    }
-
-
-    // Listen on provided port, on all network interfaces.
-    server.listen(port);
-    server.on('error', onError);
-    server.on('listening', onListening);
-
-
-    // Event listener for HTTP server "error" event.
-    function onError(error) {
-        if (error.syscall !== 'listen') {
-            throw error;
-        }
-
-        var bind = typeof port === 'string' ? 'Pipe ' + port
-            : 'Port ' + port;
-
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-            case 'EACCES':
-                console.error(bind + ' requires elevated privileges');
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                console.error(bind + ' is already in use');
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
-    }
-
-    // Event listener for HTTP server "listening" event.
-    function onListening() {
-        var addr = server.address();
-        var bind = typeof addr === 'string' ? 'pipe ' + addr
-            : 'port ' + addr.port;
-        debug('Listening on ' + bind);
-    }
-
-    return server
-
-};
-
-nodeEngine.mongo = function(server){
+    ///////////////////
+    //    MongoDB    //
+    ///////////////////
 
     // This is used for mongodb and the api stuff cross origin resource sharing
-    server.use(cors());
+        server.use(cors());
 
     // for the rest API post, put requests
-    server.use(bodyParser.urlencoded({ extended: false }));
-    server.use(bodyParser.json());
-    server.use(bodyParser.raw());
-    server.use(bodyParser.text());
+        server.use(bodyParser.urlencoded({ extended: false }));
+        server.use(bodyParser.json());
+        server.use(bodyParser.raw());
+        server.use(bodyParser.text());
 
-    return server
+
+    ////////////////////
+    // Static Assets  //
+    ////////////////////
+
+        var cacheTime = config.env.CACHETIME;
+
+        server.use(express.static('media',{ maxAge: cacheTime }));
+        server.use(express.static(path.join(__dirname, '/static'),{ maxAge: cacheTime }));
+        server.use(express.static(path.join(__dirname, '/universal/css'),{ maxAge: cacheTime }));
+        server.use(express.static(path.join(__dirname, '/universal/js'),{ maxAge: cacheTime }));
+
+
+    /////////////////////////
+    // Server Boilerplate  //
+    /////////////////////////
+
+    // Get port from environment and store in Express.
+        var port = normalizePort(process.env.PORT || config.env.PORT || '3000');
+        server.set('port', port);
+
+    //Normalize a port into a number, string, or false.
+        function normalizePort(val) {
+            var port = parseInt(val, 10);
+
+            if (isNaN(port)) {
+                // named pipe
+                return val;
+            }
+
+            if (port >= 0) {
+                // port number
+                console.log("App is running on Port: " + port);
+                return port;
+            }
+
+            return false;
+        }
+
 };
 
-nodeEngine.static = function (server, cacheTime){
 
-    server.use(express.static('media',{ maxAge: cacheTime }));
-    server.use(express.static(path.join(__dirname, '/static'),{ maxAge: cacheTime }));
-    server.use(express.static(path.join(__dirname, '/universal/css'),{ maxAge: cacheTime }));
-    server.use(express.static(path.join(__dirname, '/universal/js'),{ maxAge: cacheTime }));
-
-    return server
-
-};
+/////////////////////////
+//       Export        //
+/////////////////////////
 
 module.exports = nodeEngine;
