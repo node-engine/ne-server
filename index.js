3952e4c8379025cbd83e724eb46b8b1ea014e0a2
Bernard Hamann <bernard@yebolocal.com>
2015-09-17 19:35:38 +0200

    Removed React Engine and added a sample folder for the views engine code samples.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var debug = require('debug')('express:server');
-var path = require('path');
-var ReactEngine = require('react-engine');
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
 var cors = require('cors');
@@ -97,29 +95,6 @@ nodeEngine.init = function(currentEnv, configDevelopment, configProduction){
     }
 
     return server
-};
-
-nodeEngine.reactViews = function(server){
-
-    // create an engine instance
-    var engine = ReactEngine.server.create({
-        reactRoutes:'./routes.jsx'
-    });
-
-    // view engine setup react engine
-    server.engine('.js', engine);
-
-    // set the view directory
-    server.set('views', path.join(__dirname, '/app/universal/handlers'));
-
-    // set jsx or js as the view engine
-    // (without this you would need to supply the extension to res.render())
-    // ex: res.render('index.jsx') instead of just res.render('index').
-    server.set('view engine', 'js');
-
-    // finally, set the custom view
-    server.set('view', require('react-engine/lib/expressView'));
-
 };
 
 
