1056d0d013218c022bbdb35d95ab07bcab836c8d
Bernard Hamann <bernard@yebolocal.com>
2015-09-18 18:00:44 +0200

    api change

--- sample-server.js
+++ sample-server.js
@@ -1,28 +1,49 @@
 #!/usr/bin/env node
 
-var nodeEngine = require('node-engine-core');
-var configDevelopment = require('../config.json');
-var configProduction = require('../pm2.json');
-var path = require('path');
 var express = require('express');
+var path = require('path');
+
+var nodeEngineServer = require('ne-server');
+var renderServer = require('ne-render-server');
+
+var configDevelopment = require('../config/config.json');
+var configProduction = require('../config/pm2.json');
+var globals = require('../config/globals.json');
+var routes = require ('./universal/routes');
+
+
+////////////////////////
+// Config
+////////////////////////
+
+var currentEnv = process.env.NODE_ENV || 'development';
+console.log("Current Environment: " + currentEnv);
+
+if ('development' == currentEnv) {
+    var config = configDevelopment;
+    console.log('Using Development CONFIG');
+}
+
+if ('production' == currentEnv) {
+    var config = configProduction;
+    console.log('Using Production CONFIG');
+}
 
 
 ////////////////////////
 // Create the Server
 ////////////////////////
 
+var server = nodeEngineServer.init(config);
 
-var currentEnv = process.env.NODE_ENV || 'development';
-console.log("Current Environment: " + currentEnv);
-
-var server = nodeEngine.init(currentEnv, configDevelopment, configProduction);
-
-var config = server.locals.config;
+server.locals.globals = globals;
 
 
 /////////////////////////
 // View Engine Setup
 /////////////////////////
+
+// Optional
 
 
 //////////////////////
@@ -39,17 +60,32 @@ server.use(express.static(path.join(__dirname, '/universal/js'),{ maxAge: cacheT
 
 
 ///////////////
-// Routes
+// REST API
 ///////////////
 
-// Setup the routes for the API
-server.use('/api/people', require('./restapi/people'));
+// People Rest API
+server.use('/api/people', require('./api/people'));
 
-// Setup the routes for the Page Metadata
-server.use('/api/page', require('./restapi/page'));
+// Page Rest API
+server.use('/api/page', require('./api/page'));
 
-// Setup the routes for the Page Metadata
-server.use('/api/emails', require('./restapi/emails'));
+// Emails Rest API
+server.use('/api/emails', require('./api/emails'));
 
-// Setup the redirect to the react router
-server.use('/', require('./server/redirect'));
+
+//////////////////////////////
+// Express Test
+//////////////////////////////
+
+// Server Rendering with React Router
+server.use('/express', require('./server/express'));
+
+
+////////////////////////////////////////////////////////////
+// Rendering React-Router with Pre-Render Data
+////////////////////////////////////////////////////////////
+
+// Server Rendering with React Router but after getting data from a api request.
+// This version stores the data request in the page object for that path
+var pageAPIPath = "http://localhost:3001/api/page?f1=path&v1=";
+renderServer(server, routes, pageAPIPath, globals);
